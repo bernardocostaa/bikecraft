b80ff7d7c185f60dfde8af33636514688f691fca
Bernardo Curso <bernardo-v.f@hotmail.com>
2021-12-30 00:14:35 -0300

    galeria

--- js/script.js
+++ js/script.js
@@ -42,3 +42,22 @@ function eventosPerguntas(pergunta){
 }
 
 perguntas.forEach(eventosPerguntas);
+
+//Galeria de Bicicletas
+
+const galeria = document.querySelectorAll(".bicicletas-imagens img");
+const galeriaContainer = document.querySelector(".bicicletas-imagens");
+
+function trocarImagem(event){
+  const img = event.currentTarget;
+ const media = window.matchMedia('(min-width: 1000px)').matches;
+ if(media){
+    galeriaContainer.prepend(img);
+  }
+}
+
+function eventosGaleria(img){
+  img.addEventListener('click' , trocarImagem);
+}
+
+galeria.forEach(eventosGaleria);
